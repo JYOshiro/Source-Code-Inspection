eca157825fadc4581ace9f3373d10dacb793a4b7
41506154 <41506154@099537-P31S204.macklabs.br>
2017-11-06 19:07:29 -0200

    Alteração de main

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachineStart.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachineStart.java
@@ -1,8 +1,19 @@
 package br.calebe.ticketmachine.core;
 
 public class TicketMachineStart {
-    public static void main(String [] args){
-        PapelMoeda papel = new PapelMoeda(5,100);
-        TicketMachine ticket = new TicketMachine(100); 
+    
+    public static PapelMoeda papel2 = new PapelMoeda(2,50);
+    PapelMoeda papel5 = new PapelMoeda(5,20);
+    PapelMoeda papel10 = new PapelMoeda(10,10);
+    PapelMoeda pape20 = new PapelMoeda(20,5);
+    TicketMachine ticket = new TicketMachine(400); 
+        
+    public static void main(String [] args){        
+        
+        System.out.println("Saldo de 2 reais: " + papel2.getQuantidade());
+       
+        System.out.println("Saldo de 2 reais: " + papel2.getQuantidade());
+                
+        
     }
 }
