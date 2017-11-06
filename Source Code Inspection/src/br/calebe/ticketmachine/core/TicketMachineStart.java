9313ec4bd1714cf676d721ba1e1a9877f47fe42f
41527909 <41527909@099219-P31S204.macklabs.br>
2017-11-06 20:05:02 -0200

    Sprint 1v1 - Chamada ao Inserir de TicketMachine Sprint 3v1 - Correção de Issue #17

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachineStart.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachineStart.java
@@ -1,19 +1,27 @@
 package br.calebe.ticketmachine.core;
 
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+
 public class TicketMachineStart {
     
     public static PapelMoeda papel2 = new PapelMoeda(2,50);
-    PapelMoeda papel5 = new PapelMoeda(5,20);
-    PapelMoeda papel10 = new PapelMoeda(10,10);
-    PapelMoeda pape20 = new PapelMoeda(20,5);
-    TicketMachine ticket = new TicketMachine(400); 
+    public static PapelMoeda papel5 = new PapelMoeda(5,20);
+    public static PapelMoeda papel10 = new PapelMoeda(10,10);
+    public static PapelMoeda pape20 = new PapelMoeda(20,5);
+    public static TicketMachine machine = new TicketMachine(400); 
         
     public static void main(String [] args){        
         
         System.out.println("Saldo de 2 reais: " + papel2.getQuantidade());
-       
-        System.out.println("Saldo de 2 reais: " + papel2.getQuantidade());
-                
+           
+        try{
+        machine.inserir(10);
+        }
+        catch(PapelMoedaInvalidaException ex){
+        
+        }
+        
+        
         
     }
 }
