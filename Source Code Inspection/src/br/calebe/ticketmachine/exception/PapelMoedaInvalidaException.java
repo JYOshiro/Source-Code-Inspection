d0ca49afac2d91f79b520e1b2e346026ed2c0eaa
jessica.oshiro <jessica.oshiro@LSTKBR08493.softtek.com>
2017-11-13 10:23:42 -0200

    Alteração de exceptions

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -1,10 +1,18 @@
 package br.calebe.ticketmachine.exception;
 
+import java.awt.Toolkit;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
+    
+    protected String PapelMoedaInvalido = "Este dispositivo só aceita moeda de 2, 5, 10, 20 ,50, 100";
    
-
+    public String getPapelMoedaInvalido(){
+        return PapelMoedaInvalido;
+    }
 }
--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -1,8 +1,18 @@
 package br.calebe.ticketmachine.exception;
 
+import java.awt.Toolkit;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+      
+   protected String SaldoInsuficiente = "Saldo insuficiente, por favor completar o valor.";
+   
+   public String getSaldoInsuficiente(){
+       return SaldoInsuficiente;
+   }
 }
