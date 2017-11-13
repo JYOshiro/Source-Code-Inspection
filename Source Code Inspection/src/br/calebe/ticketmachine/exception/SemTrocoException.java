a4631adba73a219e25fc61863370b73be49e2f18
jessica.oshiro <jessica.oshiro@LSTKBR08493.softtek.com>
2017-11-13 11:26:31 -0200

    Alteração da funcionalidade Troco

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SemTrocoException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SemTrocoException.java
@@ -4,5 +4,9 @@ package br.calebe.ticketmachine.exception;
  * @author Jessica Oshiro
  */
 public class SemTrocoException extends Exception{
+    protected String SemTroco = "Sem troco";
     
+    public String getSemTroco(){
+       return SemTroco;
+   }
 }
